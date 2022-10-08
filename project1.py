82414d0a31368b0b5a6021f908cc4c8c2a8c8ab0
Chandhanu M K <chandhanu.m.k@gmail.com>
2022-10-08 13:50:56 -0400

    Update agent 1 done - QA passed

--- project1.py
+++ project1.py
@@ -1,9 +1,11 @@
+from platform import node
 from turtle import update
 import numpy as np 
 from enum import Enum
 import random
 from collections import deque, namedtuple
 import time 
+import json 
 
 PROBABLITY_BLOCKED = 0.28
 PROBABLITY_UNBLOCKED = 1-PROBABLITY_BLOCKED
@@ -18,6 +20,7 @@ UP = (0,1)
 DOWN = (-1,0)
 
 
+agent2_path = []
 
 static_i = 0
 
@@ -38,7 +41,10 @@ class Node:
         self.parent =  parent
 
 def node_to_path(node):
-        path = [node.state]
+        try:
+            path = [node.state]
+        except:
+            print(node.state)
         # work backwards from end to front
         while node.parent is not None:
             node = node.parent
@@ -59,8 +65,8 @@ class Maze:
         self.ghost_entering_block_probablity = 0.5
     
     
-    def display(self):
-        print("---MAZE---")
+    def display(self, m="MAZE"):
+        print("---"+m+"---")
         for i in self.grid:
             print(" ".join(i))
     
@@ -92,35 +98,43 @@ class Maze:
     
     def get_ghost_child(self, x):
         coordinates=[]        
-        if (x.row + 1 < self.rows) and (self.grid[x.row + 1][x.col] != Components.GHOST):
+        if (x.row + 1 < self.rows) and (self.grid[x.row + 1][x.col] != Components.GHOST) and (Index(x.row + 1,x.col) != self.goal) :
             if self.grid[x.row + 1][x.col] == Components.BLOCK:
                 if random.uniform(0, 1.0) < PROBABLITY_GHOST_ENTERING_BLOCK:
                     coordinates.append(Index(x.row + 1, x.col))
             else:
                 coordinates.append(Index(x.row + 1, x.col))
-        if  (x.row - 1 >= 0) and (self.grid[x.row - 1][x.col] != Components.GHOST):
+        if  (x.row - 1 >= 0) and (self.grid[x.row - 1][x.col] != Components.GHOST) and (Index(x.row -1 ,x.col) != self.goal) :
             if self.grid[x.row - 1][x.col] == Components.BLOCK:
                 if random.uniform(0, 1.0) < PROBABLITY_GHOST_ENTERING_BLOCK:
                     coordinates.append(Index(x.row - 1, x.col))
             else:
                 coordinates.append(Index(x.row - 1, x.col))
-        if  (x.col + 1 < self.cols) and (self.grid[x.row][x.col + 1] != Components.GHOST):
+        if  (x.col + 1 < self.cols) and (self.grid[x.row][x.col + 1] != Components.GHOST) and (Index(x.row ,x.col+1) != self.goal):
             if self.grid[x.row][x.col + 1] == Components.BLOCK:
                 if random.uniform(0, 1.0) < PROBABLITY_GHOST_ENTERING_BLOCK:
                     coordinates.append(Index(x.row, x.col + 1))
             else:
                 coordinates.append(Index(x.row, x.col + 1))
-        if (x.col - 1 >= 0) and (self.grid[x.row][x.col - 1] != Components.GHOST):
+        if (x.col - 1 >= 0) and (self.grid[x.row][x.col - 1] != Components.GHOST) and (Index(x.row,x.col-1) != self.goal) :
             if self.grid[x.row][x.col - 1] == Components.BLOCK:
                 if random.uniform(0, 1.0) < PROBABLITY_GHOST_ENTERING_BLOCK:
                     coordinates.append(Index(x.row, x.col - 1))
             else:    
                 coordinates.append(Index(x.row, x.col - 1))
+
+        if len(coordinates) == 0:
+            return x
         if len(coordinates) == 1:
             return coordinates[0]
         else:
             if len(coordinates)>0:
-                return coordinates[random.randrange(len(coordinates))]
+                #print("coo--", coordinates)
+                c = random.randrange(len(coordinates))
+                if c == self.goal and len(coordinates)>1:
+                    coordinates.pop(c)
+                    c = random.randrange(len(coordinates))
+                return coordinates[c]
         return coordinates
 
     def mark(self, path):
@@ -152,6 +166,44 @@ class Maze:
                 visited.add(child)
                 #self.grid[child.row][child.col] = "*"
                 stack.append(child)
+        return None 
+
+    def dfs_ghost_placement(self, start, destination):
+
+        stack = deque()
+        stack.append(start)
+        visited = set([start])
+        while stack:
+            curr = stack.pop()
+            #print(curr, self.goal)
+            if curr == destination:
+                return curr
+            for child in self.get_children(curr):
+                #print("curr:", curr, "children:", child.row, child.col)
+                if child in visited:
+                    continue
+                visited.add(child)
+                #self.grid[child.row][child.col] = "*"
+                stack.append(child)
+        return None 
+
+    def dfs_agent2(self, start, destination):
+        stack = deque()
+        stack.append(Node(start, None))
+        visited = set([start])
+        while stack:
+            curr = stack.pop()
+            curr_index = curr.state
+            #print(curr, self.goal)
+            if curr_index == destination:
+                return curr
+            for child in self.get_children(curr_index):
+                #print("curr:", curr, "children:", child.row, child.col)
+                if child in visited:
+                    continue
+                visited.add(child)
+                #self.grid[child.row][child.col] = "*"
+                stack.append(Node(child, curr))
         return None 
 
     def bfs(self, start, destination):
@@ -203,13 +255,18 @@ class Maze:
 
     def learn_structure(self):
         free, blocked = [], []
+        ghost_lookup_agent2 = {}
         for row in range(self.rows):
             for col in range(self.cols):
                 if self.grid[row][col] == Components.BLOCK.value:
                     blocked.append((row,col))
                 if self.grid[row][col] == Components.EMPTY.value:
                     free.append((row,col))
-        return free, blocked 
+                    is_path = self.dfs_agent2(Index(row,col), self.goal)
+                    if is_path:
+                        path = node_to_path(is_path)
+                        ghost_lookup_agent2[Index(row, col)] = path
+        return free, blocked, ghost_lookup_agent2
                     
     def initiate(self):
         #maze = self.generate_maze()
@@ -235,11 +292,14 @@ class Maze:
         while (ghost_i<self.ghost_count):
             a = random.choice(FREE_CELLS)
             i, j = a
-            if not (self.grid[i][j] == Components.GHOST.value or self.grid[i][j] == Components.BLOCK.value):
-                if self.dfs(Index(i, j), self.start)!=None:
+            #print(a, )
+            if not (self.grid[i][j] == Components.GHOST.value or self.grid[i][j] == Components.BLOCK.value or self.grid[i][j] == Components.START.value or self.grid[i][j] == Components.GOAL.value):
+                if self.dfs_ghost_placement(Index(i, j), self.start)!=None:
                     self.grid[i][j] = Components.GHOST.value
                     self.ghost_indexes[Index(i,j)] = {"replaced_value": Components.EMPTY }
                     ghost_i+=1
+                else:
+                    return None
     
     def advance_ghosts(self):
         #print("Advance ghosts")
@@ -249,9 +309,9 @@ class Maze:
         #print("------------------")
         delete_ghost_indexes = []
         create_ghost_indexes = {}
-        #self.display()
         for g_i in ghost_index_list:
             g_v = self.ghost_indexes[g_i]
+            #self.display("Advance ghosts")
             ghost_child = self.get_ghost_child(g_i)
             #print("parent:",g_i, "child:",ghost_child)
             #print("Ghost_indexes", g_i, )
@@ -287,14 +347,20 @@ class Maze:
         else:
             return None
 
+    '''def agent2_lookup(self, source, dest , lookup):
+        curr = source
+        path = []
+        is_path = self.dfs_agent2(source, dest, lookup)
+
+        pass'''
     
     def agent2(self):
         path = []
         curr = self.start
-        #while not (curr in self.ghost_indexes) and curr!=self.goal:
-        while self.grid[curr.row][curr.col] != Components.GHOST and curr!=self.goal:
+        while not (curr in self.ghost_indexes) and curr!=self.goal:
+        #while self.grid[curr.row][curr.col] != Components.GHOST and curr!=self.goal:
             self.display()
-            if self.bfs(curr, self.goal)!=None:
+            if self.dfs(curr, self.goal)!=None:
                 next_position = self.get_next_position(curr)
                 if next_position == None:
                     print("Agent is dead at 1 : ", next_position)
@@ -319,7 +385,42 @@ class Maze:
             print("Agent is dead at : ", next_position)
             print(self.ghost_indexes)
             return None
-        
+
+    def agent2_revamp(self, source): #This waits till the ghost leaves the block (Not in the descriptions)
+        path = []
+        curr = source 
+        global static_i
+        global agent2_path
+        while curr!=self.goal and (not (curr in self.ghost_indexes )):
+            #print("Iteration ", static_i, ":", "curr:",curr)
+            static_i+=1
+            path = self.dfs_agent2(curr, self.goal)
+            if path:
+                route = node_to_path(path)
+                #print(route)
+                for node in route:
+                    #self.display()
+                    #self.grid[node.row][node.col] = "A"
+                    if node in self.ghost_indexes:
+                        print("Agent 2 is killed at ", node)
+                        self.display("Agent 2 blocked")
+                        #next_position = self.get_next_position(curr)
+                        #self.agent2_revamp(next_position)
+                        #return None 
+                    else:
+                        agent2_path.append(node)
+                    self.advance_ghosts()
+                        
+                else:
+                    return path
+            else:
+                next_position = self.get_next_position(curr)
+                if next_position == None:
+                    return None
+                else:
+                    agent2_path.append(next_position)
+                self.advance_ghosts()
+                #return self.agent2_revamp(next_position)
     
     def agent1(self):
         path = []
@@ -328,7 +429,7 @@ class Maze:
         if shortest_path:
             for node in shortest_path:
                 self.display()
-                self.grid[node.row][node.col] = "A"
+                #self.grid[node.row][node.col] = "A"
                 if node in self.ghost_indexes:
                     print("Agent 1 died at ", node)
                     return None 
@@ -338,35 +439,53 @@ class Maze:
                 return shortest_path
         return None
             
-    
-
-    
-
 if __name__ == "__main__":
-    maze = Maze(rows=4, cols=4, probablity_blocked=PROBABLITY_BLOCKED, ghost_count=2)
+    maze = Maze(rows=4, cols=4, probablity_blocked=PROBABLITY_BLOCKED, ghost_count=3)
     # 1. Initiate MAZE 
     maze.initiate()
-    FREE_CELLS, BLOCKED_CELLS =  MAZE.learn_structure()
-    #print("START", MAZE.start)
-    #print("GOAL", MAZE.goal)
-    #print(MAZE.start.row, "---------")
-    # 2. Populate Ghost 
+    FREE_CELLS, BLOCKED_CELLS, ghost_lookup_agent2 =  MAZE.learn_structure()
     MAZE.populate_ghost()
     
     # 3. Display Maze 
-    MAZE.display()    
-
-    # 4. AGENT 1
-    path = MAZE.agent1()
+    MAZE.display("Initial MAZE")    
+    
+    # 4. AGENT 1 -- working
+    '''path = MAZE.agent1()
     if path:
         print(path)
     else:
-        print("Agent 1 Martyred")
-    wait = input()
+        print("Agent 1 Martyred")'''
+    #wait = input()
     #print(MAZE.ghost_indexes)
+    #path = MAZE.agent2()
+    print("Agent2 ")
+    #path = MAZE.agent2_lookup(MAZE.start, MAZE.goal, ghost_lookup_agent2)
+    #print(path)
+    '''path = MAZE.dfs_agent2(MAZE.start, MAZE.goal)
+    if path:
+        path = node_to_path(path)
+        MAZE.mark(path)
+        #print(len(path), path)
+        MAZE.display()
+        MAZE.clear(path)'''
+
     path = MAZE.agent2()
-    print(path)
-    MAZE.display()
+    #path = MAZE.agent2_revamp(MAZE.start)
+    if path:
+        path = node_to_path(path)
+        MAZE.display("Actual maze")
+        MAZE.mark(path)
+        #print(len(path), path)
+        MAZE.display()
+        MAZE.clear(path)
+        print(path)
+        print("agent2", agent2_path)
+        #print(len(path), path)
+        MAZE.clear(path)
+
+    else:
+        print("Agent 2 Martyred")
+    exit(-1)
     '''for i in range(10):
         print(i)
         MAZE.advance_ghosts()
